bd4455f9fd06c0621c6f6c9cb87849a849a00c3c
azizdev <30.azizbek@gmail.com>
2024-06-20 15:39:50 +0500

    Initial commit

---
+++ src/main/java/dev/aziz/amazonrestapi/AmazonRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.aziz.amazonrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonRestApiApplication.class, args);
+    }
+
+}
